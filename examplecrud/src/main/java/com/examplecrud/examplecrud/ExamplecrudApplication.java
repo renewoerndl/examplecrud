432efa7ecd37fefb6b48c737bf36c5b59b9a8768
woerndlr <rene.woerndl@austriacard.at>
2025-02-14 08:35:31 +0100

    initial commit

---
+++ examplecrud/src/main/java/com/examplecrud/examplecrud/ExamplecrudApplication.java
@@ -0,0 +1,13 @@
+package com.examplecrud.examplecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamplecrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamplecrudApplication.class, args);
+	}
+
+}
---
+++ examplecrud/src/test/java/com/examplecrud/examplecrud/ExamplecrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplecrud.examplecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamplecrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
